e60d7cbc107fe1643505f2d8a6fa5d99959808ee
Luiz Oliveira <ziuloliveira@gmail.com>
2015-04-03 14:37:09 -0300

    Test for parser.read_file
    Signed-off-by: Luiz Oliveira <ziuloliveira@gmail.com>

--- app/tests.py
+++ app/tests.py
@@ -12,3 +12,12 @@ class APP_Parser(Parser_Test):
 
         parser = Parser()
         self.assertIsNotNone(parser)
+
+    def test_parser_read_file(self):
+
+        parser = Parser()
+        print ''
+        self.assertRaises(IOError, parser.read_file, '')
+        self.assertRaises(IOError, parser.read_file, 'away')
+        file = 'importer/data/bus_lines.csv'
+        self.assertIsNotNone(parser.read_file(file))
