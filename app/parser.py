2c8572993791baf89226bf38cd53086d1780f8a9
Luiz Oliveira <ziuloliveira@gmail.com>
2015-04-03 14:51:22 -0300

    Fix import and cut verbose
    Signed-off-by: Luiz Oliveira <ziuloliveira@gmail.com>

--- app/parser.py
+++ app/parser.py
@@ -7,17 +7,17 @@ from api.models import BusLine
 class Parser():
 
     def read_file(self, file_name):
-        print "Opening csv file"
+        print "Opening file '%s'" % file_name
         csv_file = open(file_name)
         csv_file = csv.reader(csv_file, delimiter=',', quotechar="'")
 
         return csv_file
 
     def import_bus_lines(self):
-        csv_file = self.read_file('app/data/bus_lines.csv')
+        csv_file = self.read_file('importer/data/bus_lines.csv')
 
         for row in csv_file:
-            print 'Importing line: ', row[0]
+            # print 'Importing line: ', row[0]
             bus_line = BusLine()
             bus_line.line_number = row[0]
             bus_line.description = row[1]
