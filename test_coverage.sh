763ece0fd9e312cb2797009dc7a61c90491ea684
Luiz Oliveira <ziuloliveira@gmail.com>
2015-04-06 09:34:29 -0300

    Removed app folder
    Signed-off-by: Luiz Oliveira <ziuloliveira@gmail.com>

--- test_coverage.sh
+++ test_coverage.sh
@@ -2,19 +2,11 @@
 echo -n "Removing *.pyc ------------------------------------------ "
 find . -type f -name "*.pyc" -exec rm -rf "{}" \;
 echo "Done"
-echo -n "Initing server ------------------------------------------ "
-python manage.py runserver 8000 &> /dev/null  &
-echo "Done"
 coverage erase
 echo "API Tests"
 coverage run --source='api' manage.py test api
 coverage report -m
 coverage html -d ../coverage/API
-echo ""
-echo "APP Tests"
-coverage run --source='app' manage.py test app
-coverage report -m
-coverage html -d ../coverage/APP
 echo -n "Killing everything -------------------------------------- "
 killall -9 coverage python test_coverage.sh -q
 echo "Killed"
