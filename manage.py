c63ddad3ab6df1b53f3800d143debcc633fdaa44
msfernandes <matheus.souza.fernandes@gmail.com>
2015-04-11 23:43:19 -0300

    Remove admin.py and set manage.py to delete *.pyc

--- manage.py
+++ manage.py
@@ -5,6 +5,10 @@ import sys
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "configuration.settings")
 
+    if (len(sys.argv) >= 2) and (sys.argv[1] == 'test'):
+        print 'Removing *.pyc'
+        os.system('find . -type f -name "*.pyc" -exec rm -rf "{}" \;')
+
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
